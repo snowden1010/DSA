b7d92a45ab3df102b9340dd45dee2c864346213d
snowden1010 <abdoiddaoudi050@gmail.com>
2024-09-14 16:34:41 +0100

    "Linear_search Functional."

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+def linear_search(array, target):
+    for item in array:
+        if item == target:
+            return f"Found {target} at {array.index(item)}"
 
-print("Hello World")
+    return f"{target} doesn't exist"
+
+
+arr = [i for i in range(0, 103, 3)]
+target = arr[-1]
+print(linear_search(arr, target))
