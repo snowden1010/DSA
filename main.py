e0e2a3b5bdab62edcf6fb77f426d8501830f8fa1
snowden1010 <abdoiddaoudi050@gmail.com>
2024-09-14 16:43:43 +0100

    "refactored variables"

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def linear_search(array, target):
+def linear_search(array, r):
     for item in array:
-        if item == target:
-            return f"Found {target} at {array.index(item)}"
-            wrong;
-    return f"{target} doesn't exist"
+        if item == r:
+            return f"Found {r} at {array.index(item)}"
+    return f"{r} doesn't exist"
 
 
 arr = [i for i in range(0, 103, 3)]
